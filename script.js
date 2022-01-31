4bae58cb7d70a305e832e4c43935be5d56bdfc19
mooshkid <mooshkid@gmail.com>
2022-01-31 17:50:14 +0900

    Updated

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ window.addEventListener('mouseup', function(event){
         backdrop.classList.remove("active");
     }
 });
+
+// DISABLE CSS TRANSITION ON MEDIA QUERY 
+stopResponsiveTransition();
+function stopResponsiveTransition() {
+    const classes = document.body.classList;
+    let timer = null;
+    window.addEventListener('resize', function () {
+    if (timer){
+        clearTimeout(timer);
+        timer = null;
+    }else {
+        classes.add('stop-transition');
+    }
+    timer = setTimeout(() => {
+        classes.remove('stop-transition');
+        timer = null;
+    }, 100);
+    });
+}
