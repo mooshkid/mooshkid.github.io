38136879b8d5614ba85221c2ff9864e7295c7c0b
mooshkid <mooshkid@gmail.com>
2022-06-23 13:55:08 +0900

    Added Scroll Next Jquery

--- script.js
+++ script.js
@@ -280,3 +280,51 @@ window.onload = function () {
     });
     return;
 };
+
+
+// Scroll to next section on click 'anchor.png'
+// This uses jquery :(
+$(function(){
+    
+    var pagePositon = 0,
+        sectionsSeclector = 'section',
+        $scrollItems = $(sectionsSeclector),
+        offsetTolorence = 30,
+        pageMaxPosition = $scrollItems.length - 1;
+    
+    //Map the sections:
+    $scrollItems.each(function(index,ele) { $(ele).attr("debog",index).data("pos",index); });
+
+    // Bind to scroll
+    $(window).bind('scroll',upPos);
+    
+    //Move on click:
+    $('#anchor a').click(function(e){
+        if ($(this).hasClass('next') && pagePositon+1 <= pageMaxPosition) {
+            pagePositon++;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top
+            }, 300);
+        }
+        if ($(this).hasClass('previous') && pagePositon-1 >= 0) {
+            pagePositon--;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top
+              }, 300);
+            return false;
+        }
+    });
+    
+    //Update position func:
+    function upPos(){
+       var fromTop = $(this).scrollTop();
+       var $cur = null;
+        $scrollItems.each(function(index,ele){
+            if ($(ele).offset().top < fromTop + offsetTolorence) $cur = $(ele);
+        });
+       if ($cur != null && pagePositon != $cur.data('pos')) {
+           pagePositon = $cur.data('pos');
+       }                   
+    }
+    
+});
